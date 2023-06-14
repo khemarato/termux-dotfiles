13cfd62d75dbffbf56318e665269b10dfe1f6bbe
Khemarato Bhikkhu <theopenbuddhistuniversity@gmail.com>
2023-06-14 11:31:58 +0700

    shortcut imps

--- .shortcuts/normalize-lg-names.sh
+++ .shortcuts/normalize-lg-names.sh
@@ -1,7 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-source /data/data/com.termux/files/home/termux-dotfiles/functions.bash
 cd /data/data/com.termux/files/home/storage/shared/Download/
-normalizeLGNames
-read -p "Enter to exit..." foo
+
+# remove anything in ()s (space before or after)
+perl-rename -v "s/\([a-zA-Z0-9 _\.-]*\) //g" *
+perl-rename -v "s/ \([a-zA-Z0-9 _\.-]*\)//g" *
+# remove "-Publisher" from end
+perl-rename -v 's/([a-z])-[A-Z][a-zA-Z 0-9]+\.(cbz|epub|pdf)/$1.$2/g' *
+# flip from "Author - Title.ext" to "Title - Author.ext"
+perl-rename -v 's/(.*) - (.*)\.([a-z0-9]+)$/$2 - $1.$3/g' *
 
--- functions.bash
+++ functions.bash
@@ -51,16 +51,6 @@ mergeThesePdfsCmd() {
   echo "$ret -- '$outf'"
 }
 
-normalizeLGNames() {
-  # remove anything in ()s (space before or after)
-  perl-rename -v "s/\([a-zA-Z0-9 _\.-]*\) //g" *
-  perl-rename -v "s/ \([a-zA-Z0-9 _\.-]*\)//g" *
-  # remove "-Publisher" from end
-  perl-rename -v 's/([a-z])-[A-Z][a-zA-Z 0-9]+\.(cbz|epub|pdf)/$1.$2/g' *
-  # flip from "Author - Title.ext" to "Title - Author.ext"
-  perl-rename -v 's/(.*) - (.*)\.([a-z0-9]+)$/$2 - $1.$3/g' *
-}
-
 SELECTOUTFILEPREFIX=select-pages-from-
 pdfselect() {
   # Uses poppler utils to extract specific pages
