635e404177d3363867dcd59081e81c4bbff41cfb
Khemarato Bhikkhu <theopenbuddhistuniversity@gmail.com>
2024-02-29 18:26:58 +0700

    shortcuts

---
+++ .shortcuts/go-through.sh
@@ -0,0 +1,3 @@
+#!/data/data/com.termux/files/usr/bin/bash
+python3 /data/data/com.termux/files/home/storage/shared/Documents/buddhist-uni.github.io/scripts/android_go_through.py
+
---
+++ .shortcuts/rm-gdupes.sh
@@ -0,0 +1,3 @@
+#!/data/data/com.termux/files/usr/bin/bash
+python3 /data/data/com.termux/files/home/storage/shared/Documents/buddhist-uni.github.io/scripts/delete_uploaded_files.py /data/data/com.termux/files/home/storage/shared/Download -r -y
+
