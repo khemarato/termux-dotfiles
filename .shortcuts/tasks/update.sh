7d098e837759fd9dad2cc2447da51562067e57c1
Khemarato Bhikkhu <theopenbuddhistuniversity@gmail.com>
2024-06-13 10:28:49 +0700

    Mastodon.py

--- .shortcuts/tasks/update.sh
+++ .shortcuts/tasks/update.sh
@@ -6,6 +6,6 @@ timeout 1500 pkg upgrade -y
 [ $? -gt 0 ] && echo "ERROR: pkg upgrade timeout" && dpkg --configure -a --force-confnew
 apt autoremove -y
 pkg autoclean
-/data/data/com.termux/files/usr/bin/python3 -m pip install --upgrade regex pathvalidate unidecode ipython feedreader yaspin google google-api-python-client google_auth_oauthlib joblib youtube-transcript-api youtube-dl pypdf titlecase pyyaml ebooklib python-slugify python-frontmatter lxml beautifulsoup4 threadpoolctl wheel cython setuptools nltk;
+/data/data/com.termux/files/usr/bin/python3 -m pip install --upgrade regex pathvalidate unidecode ipython feedreader yaspin google google-api-python-client google_auth_oauthlib joblib youtube-transcript-api youtube-dl pypdf titlecase pyyaml ebooklib python-slugify python-frontmatter lxml beautifulsoup4 threadpoolctl wheel cython setuptools nltk Mastodon.py;
 [ $alreadylocked = "false" ] && termux-wake-unlock
 
