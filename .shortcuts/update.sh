63f2efc68415bd1b2a61c55b2ad1a7941cf6e606
Khemarato Bhikkhu <theopenbuddhistuniversity@gmail.com>
2024-10-21 18:31:18 +0700

    update

--- .shortcuts/update.sh
+++ .shortcuts/update.sh
@@ -2,7 +2,7 @@
 alreadylocked="false"
 termux-notification-list | jq -r '.[] | select(.id == 1337).content' | grep -qF 'wake lock held' && alreadylocked="true"
 [ $alreadylocked = "false" ] && termux-wake-lock
-timeout 1500 pkg upgrade -y
+timeout 1500 pkg upgrade
 [ $? -gt 0 ] && echo "ERROR: pkg upgrade timeout" && dpkg --configure -a --force-confnew
 apt autoremove -y
 pkg autoclean
